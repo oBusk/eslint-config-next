e203fd2cdf75d03a071386b668504af6ddf0cb6e
Oscar Busk <oscar.busk@gmail.com>
2025-03-02 02:29:53 +0100

    chore: upgrade deps and add ESM support
    - Update Node.js engine requirement to v22.x
    - Add ESM support with type: module
    - Upgrade dependencies to latest versions
    - Add @eslint/eslintrc dependency
    - Add utils.js with getJestVersion helper
    - Update typescript peer dep to ~5.7.0

--- README.md
+++ README.md
@@ -10,6 +10,18 @@ npm install --save-dev @obusk/eslint-config-next
 
 ## Usage
 
+### ESLint Flat Config (recommended)
+
+This package exports a flat config by default. Add the following to your `eslint.config.js`:
+
+```js
+import obuskNext from "@obusk/eslint-config-next";
+
+export default [...obuskNext];
+```
+
+### Legacy Config
+
 Add the following to your `.eslintrc` file:
 
 ```json
--- index.js
+++ index.js
@@ -1,89 +1,87 @@
-/** @type {import("eslint").ESLint.ConfigData} */
-module.exports = {
-  root: true,
-  extends: [
-    "next",
-    "next/core-web-vitals",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:tailwindcss/recommended",
-    "plugin:jsdoc/recommended-typescript-flavor-error",
-    "plugin:prettier/recommended",
-  ],
-  plugins: ["jest", "testing-library", "@typescript-eslint/eslint-plugin"],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "error",
-      {
-        prefer: "type-imports",
-        disallowTypeAnnotations: true,
-        fixStyle: "inline-type-imports",
-      },
+import { FlatCompat } from "@eslint/eslintrc";
+import { getJestVersion } from "./utils.js";
+
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
+
+const eslintConfig = [
+  ...compat.config({
+    root: true,
+    extends: [
+      "next",
+      "next/core-web-vitals",
+      "plugin:import/recommended",
+      "plugin:import/typescript",
+      "plugin:tailwindcss/recommended",
+      "plugin:jsdoc/recommended-typescript-flavor-error",
+      "plugin:prettier/recommended",
     ],
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { argsIgnorePattern: "^_", ignoreRestSiblings: true },
-    ],
-    "import/order": [
-      "error",
-      {
-        groups: [
-          ["builtin", "external"],
-          "internal",
-          "parent",
-          "sibling",
-          "index",
-        ],
-        pathGroups: [
-          {
-            pattern: "^/**",
-            group: "internal",
-          },
-        ],
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
+    plugins: ["jest", "testing-library", "@typescript-eslint/eslint-plugin"],
+    rules: {
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+          disallowTypeAnnotations: true,
+          fixStyle: "inline-type-imports",
         },
-        "newlines-between": "never",
-        warnOnUnassignedImports: true,
-      },
-    ],
-    "react/jsx-no-leaked-render": "error",
-    // Sort imports to sort import members
-    "sort-imports": [
-      "error",
-      { ignoreCase: true, ignoreDeclarationSort: true },
-    ],
-  },
-  overrides: [
-    {
-      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-      extends: [
-        "plugin:jest/recommended",
-        "plugin:jest/style",
-        "plugin:testing-library/react",
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", ignoreRestSiblings: true },
+      ],
+      "import/order": [
+        "error",
+        {
+          groups: [
+            ["builtin", "external"],
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          pathGroups: [{ pattern: "^/**", group: "internal" }],
+          alphabetize: { order: "asc", caseInsensitive: true },
+          "newlines-between": "never",
+          warnOnUnassignedImports: true,
+        },
+      ],
+      "react/jsx-no-leaked-render": "error",
+      // Sort imports to sort import members
+      "sort-imports": [
+        "error",
+        { ignoreCase: true, ignoreDeclarationSort: true },
       ],
     },
-    {
-      files: ["**/*.ts?(x)"],
-      extends: ["plugin:jsdoc/recommended-typescript-error"],
-      rules: {
-        "jsdoc/no-undefined-types": 1,
-        "jsdoc/require-jsdoc": 0,
-        "jsdoc/require-param": 0,
-        "jsdoc/require-returns": 0,
-        "jsdoc/require-yields": 0,
-        "jsdoc/tag-lines": ["error", "any", { startLines: 1, endLines: 0 }],
+    overrides: [
+      {
+        files: [
+          "**/__tests__/**/*.[jt]s?(x)",
+          "**/?(*.)+(spec|test).[jt]s?(x)",
+        ],
+        extends: [
+          "plugin:jest/recommended",
+          "plugin:jest/style",
+          "plugin:testing-library/react",
+        ],
       },
+      {
+        files: ["**/*.ts?(x)"],
+        extends: ["plugin:jsdoc/recommended-typescript-error"],
+        rules: {
+          "jsdoc/no-undefined-types": 1,
+          "jsdoc/require-jsdoc": 0,
+          "jsdoc/require-param": 0,
+          "jsdoc/require-returns": 0,
+          "jsdoc/require-yields": 0,
+          "jsdoc/tag-lines": ["error", "any", { startLines: 1, endLines: 0 }],
+        },
+      },
+    ],
+    settings: {
+      tailwindcss: { callees: ["clsx", "cx", "cva", "twMerge"] },
+      jest: { version: getJestVersion() },
     },
-  ],
-  settings: {
-    tailwindcss: {
-      callees: ["clsx", "cx", "cva", "twMerge"],
-    },
-    jest: {
-      // Trick to resolve jest from where executed (not from this project's node_modules)
-      version: require("jest/package.json").version,
-    },
-  },
-};
+  }),
+];
+
+export default eslintConfig;
